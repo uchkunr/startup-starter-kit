5e8526e971daf7d1c6147c643217a6975d6bd373
Uchkun Rakhimov <uchkunrakhimov@gmail.com>
2026-09-12 18:44:29 +0500

    fix(ci): add fallback for DATABASE_URL and configure CI env in ci.yml

--- apps/api/src/generated/client/models.ts
+++ apps/api/src/generated/client/models.ts
@@ -8,6 +8,6 @@
  *
  * 🟢 You can import this file directly.
  */
-export type * from './models/User.ts'
-export type * from './models/Session.ts'
-export type * from './commonInputTypes.ts'
+export type * from './models/User.js'
+export type * from './models/Session.js'
+export type * from './commonInputTypes.js'
